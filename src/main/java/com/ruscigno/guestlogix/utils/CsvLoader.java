c1e528004d2a7f59833793165082c4b70dcae812
Sander Ruscigno <ruscigno@gmail.com>
2019-05-19 21:57:01 -0300

    fix: classpath resource loader

--- src/main/java/com/ruscigno/guestlogix/utils/CsvLoader.java
+++ src/main/java/com/ruscigno/guestlogix/utils/CsvLoader.java
@@ -1,12 +1,13 @@
 package com.ruscigno.guestlogix.utils;
-import java.io.File;
+
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
@@ -15,17 +16,24 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 public class CsvLoader {
 
 	private static final Logger log = LoggerFactory.getLogger(CsvLoader.class);
-	
-	public static <T> List<T> loadObjectList(Class<T> type,  String fileName) {
+
+	public static <T> List<T> loadObjectList(Class<T> type, String fileName) {
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext();
 		try {
-			CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
-			CsvMapper mapper = new CsvMapper();
-			File file = new ClassPathResource(fileName).getFile();
-			MappingIterator<T> readValues = mapper.readerFor(type).with(bootstrapSchema).readValues(file);
-			return readValues.readAll();
-		} catch (IOException e) {
-			log.error("Error occurred while loading object list from file " + fileName, e);
-			return Collections.emptyList();
+			try {
+				CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
+				CsvMapper mapper = new CsvMapper();
+				Resource file = appContext.getResource("classpath:" + fileName);
+				MappingIterator<T> readValues = mapper.readerFor(type).with(bootstrapSchema)
+						.readValues(file.getInputStream());
+				return readValues.readAll();
+			} catch (IOException e) {
+				log.error("Error occurred while loading object list from file " + fileName, e);
+				return Collections.emptyList();
+			}
+		} finally {
+			appContext.close();
 		}
+
 	}
 }
